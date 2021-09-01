824cc64b47af068b680f25f621c591f105dc917d
booklover4177 <anne.straub@gmail.com>
2021-09-01 13:17:33 -0400

    created super duper counter

--- src/app/counter-list/counter-list.component.ts
+++ src/app/counter-list/counter-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Counter } from '../counter';
 import { SuperCounter } from '../super-counter';
+import { SuperDuperCounter } from '../super-duper-counter';
 
 @Component({
   selector: 'app-counter-list',
@@ -12,6 +13,7 @@ export class CounterListComponent implements OnInit {
   public name: string = "Counter Wall";
   counters: Counter[] = [];
   superCounters: SuperCounter[] = [];
+  superDuperCounters: SuperDuperCounter[]=[];
 
   constructor() { }
 
@@ -20,19 +22,41 @@ export class CounterListComponent implements OnInit {
     if (this.counters.length < 3) {
       let newCounter = new Counter();
       this.counters.push(newCounter);
-    } else {
-      let counterVal1 = this.counters[0].value;
-      let counterVal2 = this.counters[1].value;
-      let counterVal3 = this.counters[2].value;
+    } else if((this.counters.length=== 3) && (this.superCounters.length===2)){
+      this.createSuperCounter();
+      this.createSuperDuperCounter();
 
-      let newSuperCounter = new SuperCounter(counterVal1, counterVal2, counterVal3);
-      this.superCounters.push(newSuperCounter);
-      this.counters.pop();
-      this.counters.pop();
-      this.counters.pop();
+    }else{
+
+      this.createSuperCounter();
+
     }
 
 
+  }
+
+  createSuperCounter(){
+    let counterVal1 = this.counters[0].value;
+    let counterVal2 = this.counters[1].value;
+    let counterVal3 = this.counters[2].value;
+
+    let newSuperCounter = new SuperCounter(counterVal1, counterVal2, counterVal3);
+    this.superCounters.push(newSuperCounter);
+    this.counters.pop();
+    this.counters.pop();
+    this.counters.pop();
+  }
+
+  createSuperDuperCounter(){
+    let counterVal1 = this.superCounters[0].value2;
+    let counterVal2 = this.superCounters[1].value2;
+    let counterVal3 = this.superCounters[2].value2;
+
+    let newSuperDuperCounter = new SuperDuperCounter(counterVal1, counterVal2, counterVal3);
+    this.superDuperCounters.push(newSuperDuperCounter);
+    this.superCounters.pop();
+    this.superCounters.pop();
+    this.superCounters.pop();
   }
 
   subtract() {
---
+++ src/app/super-duper-counter.ts
@@ -0,0 +1,7 @@
+export class SuperDuperCounter {
+  public value3:number;
+
+  constructor(val1:number, val2:number, val3:number){
+    this.value3=val1+val2+val3;
+  }
+}
