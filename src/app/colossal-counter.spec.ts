5818c7996047c9c1e08bd15c967b2af68777aa41
booklover4177 <anne.straub@gmail.com>
2021-09-01 16:58:10 -0400

    added value to colossalCounter class and constructor

--- src/app/colossal-counter.spec.ts
+++ src/app/colossal-counter.spec.ts
@@ -2,6 +2,6 @@ import { ColossalCounter } from './colossal-counter';
 
 describe('ColossalCounter', () => {
   it('should create an instance', () => {
-    expect(new ColossalCounter()).toBeTruthy();
+    expect(new ColossalCounter(0,0,0)).toBeTruthy();
   });
 });
--- src/app/colossal-counter.ts
+++ src/app/colossal-counter.ts
@@ -1,2 +1,7 @@
 export class ColossalCounter {
+  public value4:number;
+
+  constructor(val1:number, val2:number, val3:number){
+    this.value4=val1+val2+val3;
+  }
 }
